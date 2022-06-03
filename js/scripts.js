1102542edb600c09e1a881f59f70fabbe96da143
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-03 13:00:48 -0700

    add function to add together all text boxes

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function add(number1, number2, number3, number4, number5) {
+  return number1 + number2 + number3 + number4 + number5;
+}
+
+$(document).ready(function() {
+  $("form#suggestion").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const number3 = parseInt($("#add3").val());
+    const number4 = parseInt($("#add4").val());
+    const number5 = parseInt($("#add5").val());
+    const result = add(number1, number2, number3, number4, number5);
+    $("#answer").text(result);
+  });
+});
